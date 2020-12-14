d608773650f610d0038f4fa4b3dbf14dcd7079d5
jinShine <seungjin429@gmail.com>
2020-12-15 00:46:27 +0900

    create project

---
+++ TwitterClone/Controller/MainTabController.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabController.swift
+//  TwitterClone
+//
+//  Created by buzz on 2020/12/15.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    view.backgroundColor = .systemPink
+  }
+
+}
