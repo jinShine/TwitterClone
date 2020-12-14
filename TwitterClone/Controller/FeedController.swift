9ee97e4112a76f817c37c8f1926da76648f3f48a
jinShine <seungjin429@gmail.com>
2020-12-15 02:04:07 +0900

    setting up sticky action button

--- TwitterClone/Controller/FeedController.swift
+++ TwitterClone/Controller/FeedController.swift
@@ -22,7 +22,7 @@ final class FeedController: UIViewController {
   // MARK: - Helpers
   
   func configureUI() {
-    view.backgroundColor = .red
+    view.backgroundColor = .white
     
     let imageView = UIImageView(image: UIImage(named: "twitter_logo_blue"))
     imageView.contentMode = .scaleAspectFit
--- TwitterClone/Controller/MainTabController.swift
+++ TwitterClone/Controller/MainTabController.swift
@@ -11,15 +11,38 @@ class MainTabController: UITabBarController {
   
   // MARK: - Properties
   
+  let actionButton: UIButton = {
+    let button = UIButton(type: .system)
+    button.tintColor = .white
+    button.backgroundColor = UIColor.twitterBlue
+    button.setImage(UIImage(named: "new_tweet"), for: .normal)
+    button.addTarget(self, action: #selector(actionButtonTapped), for: .touchUpInside)
+    return button
+  }()
+  
   // MARK: - Lifecycle
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
     configureViewController()
+    configureUI()
+  }
+  
+  // MARK: - Selectors
+  
+  @objc func actionButtonTapped() {
+    print(123)
   }
   
   // MARK: - Helpers
+  
+  func configureUI() {
+    view.addSubview(actionButton)
+    actionButton.anchor(bottom: view.safeAreaLayoutGuide.bottomAnchor, right: view.rightAnchor,
+                        paddingBottom: 64, paddingRight: 16, width: 56, height: 56)
+    actionButton.layer.cornerRadius = 56 / 2
+  }
   
   func configureViewController() {
     let feed = FeedController()
